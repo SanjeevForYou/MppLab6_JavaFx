ce2564d607b62abfc9e762831ffdc0589428901f
SanjeevForYou <yoursanjeev0@gmail.com>
2016-05-31 21:37:22 -0500

    first commit

---
+++ src/com/lab6/problem3/DomException.java
@@ -0,0 +1,13 @@
+package com.lab6.problem3;
+public class DomException extends Exception {
+
+    public DomException() {
+        super();
+    }
+
+    public DomException(String s) {
+        super(s);
+    }
+
+}
+
